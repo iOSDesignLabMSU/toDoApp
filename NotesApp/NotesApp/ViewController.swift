745d914be7791f6fcdcad593f98c7a2095da98fe
Malik <arachich@msu.edu>
2021-03-22 12:58:17 -0400

    Table View is showing part 1

--- NotesApp/NotesApp/ViewController.swift
+++ NotesApp/NotesApp/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(NotesTableViewCell.self, forCellReuseIdentifier: NotesTableViewCell.reUse)
+        tableView.rowHeight = 60
+        tableView.separatorColor = .black
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpView()
     }
-
-
+    
+    func setUpView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        title = "Notes"
+        view.backgroundColor = .white
+        
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NotesTableViewCell.reUse) as! NotesTableViewCell
+        cell.titleLabel.text = "Title"
+        cell.subTitleLabel.text = "Subtitle"
+        return cell
+    }
+    
+    
+}
